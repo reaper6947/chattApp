1db65de19ccd1d9af1e453dd7d5a876794f478a0
mahid <mo47da@gmail.com>
2020-03-29 12:13:28 +0600

    random user id

--- public/chat.js
+++ public/chat.js
@@ -3,30 +3,40 @@ var formEl = document.getElementById("chatForm");
 var textInputEl = document.getElementById("txt");
 var messagesEl = document.getElementById("messages");
 
-formEl.addEventListener("submit", e => {
-    e.preventDefault();
-    socket.emit("chat_message", textInputEl.value);
-    textInputEl.value = "";
-    return false;
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  socket.emit("chat_message", textInputEl.value);
+  textInputEl.value = "";
+  return false;
 });
 
 // append the chat text message
 socket.on("chat_message", function(msg) {
-    let child = document.createElement("li");
-    child.classList.add("list-group-item", "list-group-item-dark","overflow-auto");
-    child.innerHTML = msg;
-    messagesEl.appendChild(child);
-
+  let child = document.createElement("li");
+  child.classList.add(
+    "list-group-item",
+    "list-group-item-dark",
+    "overflow-auto"
+  );
+  child.innerHTML = msg;
+  messagesEl.appendChild(child);
 });
 
 // append text if someone is online
 socket.on("is_online", function(username) {
-    let child = document.createElement("li");
-    child.classList.add("mx-auto","joined-cl");
-    child.innerHTML = username;
-    messagesEl.appendChild(child);
+  let child = document.createElement("li");
+  child.classList.add("mx-auto", "joined-cl");
+  child.innerHTML = username;
+  messagesEl.appendChild(child);
 });
 
 // ask username
+
 var username = prompt("Please tell me your name");
+
+if (username == "" || username == null || username == undefined || username == "undefined") {
+    let mat = Math.floor(Math.random() * 10) + 1;
+    let str = Math.random().toString(36).substr(2, 3)
+  var username = `user-${mat}${str}`;
+} 
 socket.emit("username", username);
