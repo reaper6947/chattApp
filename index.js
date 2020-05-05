782a644527a821cd2a5bdd4c80539f995682172d
mahid <mo47da@gmail.com>
2020-05-06 00:13:09 +0600

    going back

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 var path = require("path");
-var bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
+const name = require("./username");
+// Specify a directory to serve static files
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
-var dbUrl =
-  "mongodb+srv://admin:uHENT4tDAZnliV0T@chatapp-mlab-tvvyd.gcp.mongodb.net/test";
+let dbUrl ="mongodb+srv://admin:uHENT4tDAZnliV0T@chatapp-mlab-tvvyd.gcp.mongodb.net/test";
 const connect = mongoose.connect(
   dbUrl,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -25,7 +26,7 @@ const chatSchema = new Schema(
     message: {
       type: String,
     },
-    sender: {
+    username: {
       type: String,
     },
   },
@@ -33,57 +34,40 @@ const chatSchema = new Schema(
     timestamps: true,
   }
 );
-
 let Chat = mongoose.model("theChat", chatSchema);
+//sends the previous messages to client
 
-
-
-// Specify a directory to serve static files
-app.use(express.static("public"));
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
 
 io.sockets.on("connection", function (socket) {
   socket.on("username", function (username) {
-    if (
-      username === "" ||
-      username === null ||
-      username === undefined ||
-      username === "undefined"
-    ) {
-      let mat = Math.floor(Math.random() * 10) + 1;
-      let str = Math.random().toString(36).substr(2, 3);
-      var userId = `user-${mat}${str}`;
-      socket.username = userId;
-    } else {
-      socket.username = username;
-    }
-
-    io.emit("is_online", socket.username);
+    //validating username
+    socket.username = name.validUser(username);
+    io.emit("is_online", " <i>" + socket.username + " joined the chat..</i>");
   });
 
   socket.on("disconnect", function (username) {
     io.emit("is_online", " <i>" + socket.username + " left the chat..</i>");
   });
+
   socket.on("typing", function (data) {
+    console.log(data.length, data);
     io.emit("typing", data);
   });
 
   socket.on("chat_message", function (message) {
-    io.emit("chat_message", "<b>" + socket.username + "</b>: " + message);
-    
-    connect.then((db) => {
-      console.log(`${socket.username}:${message}`);
-      let chatMessage = new Chat({ message: message, sender: socket.username });
-
-      chatMessage.save();
+    io.emit("chat_message", "" + socket.username + ": " + message);
+  });
+  app.get("/chat",(req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    connect.then(db => {
+        let data = Chat.find({});
+      Chat.find({}).then(chat => {
+        res.json(chat);
+      });
     });
-    
   });
 });
-
 const port = 3000 || process.env.PORT;
 const server = http.listen(port, function () {
   console.log(`listening on *:${port}`);
--- public/chat.js
+++ public/chat.js
@@ -2,17 +2,66 @@ var socket = io.connect("localhost:3000");
 var formEl = document.getElementById("chatForm");
 var textInputEl = document.getElementById("txt");
 var messagesEl = document.getElementById("messages-ul");
-var typingEl = document.getElementById("main-head-i");
-formEl.addEventListener("submit", (e) => {
+var typingEl = document.getElementById("main-head-i")
+
+
+//gets previous messsages from server
+  
+(function () {
+  fetch("/chat")
+    .then((data) => {
+      return data.json();
+    })
+    .then((json) => {
+      json.map((data) => {
+        child = document.createElement("li");
+       console.log(data);
+        child.classList.add(
+          "list-group-item",
+          "list-group-item-dark",
+          "overflow-auto",
+          "d-flex",
+          "border-0",
+          "message-cl"
+        );
+        child.innerHTML = `${data.username}:${data.message}`;
+        messagesEl.appendChild(child);
+      });
+    });
+})();
+
+// sends info about typing to server
+textInputEl.addEventListener("input", function () {
+  let vali = textInputEl.value.trim();
+  
+  if (vali != "") {
+   // console.log("after " +vali.length);
+    socket.emit("typing", username);
+  } else {
+    socket.emit("typing", "");
+  }
+});
+
+//receive info about user typing
+socket.on("typing", function (data) {
+  if (data.length != 0 && data != username) {
+    typingEl.innerText = `${data} is typing`;
+  } else  {
+    typingEl.innerText = "";
+  }
+});
+
+formEl.addEventListener("submit", e => {
   e.preventDefault();
   var sendMsg = textInputEl.value.trim();
   if (sendMsg != 0) {
     socket.emit("chat_message", textInputEl.value);
-    socket.emit("typing", "");
+    socket.emit('typing',"");
     textInputEl.value = "";
     return false;
   }
 });
+
 
 // append the chat text message
 socket.on("chat_message", function (msg) {
@@ -28,38 +77,36 @@ socket.on("chat_message", function (msg) {
   child.innerHTML = msg;
   messagesEl.appendChild(child);
 });
-// sends info about typing to server
-textInputEl.addEventListener("input", function () {
-  let validate = textInputEl.value.trim();
-  console.log(validate);
-  if (validate != "") {
-    console.log(validate.length);
-    socket.emit("typing", username);
-  } else {
-    socket.emit("typing", "");
-  }
-});
-//receive info about user typing
-socket.on("typing", function (data) {
-  if (data.length != 0 && data != username) {
-    typingEl.innerText = `${data} is typing`;
-  } else if (data.length <= 0) {
-    typingEl.innerText = "";
-  }
-});
+
 
 // append text if someone is online
 socket.on("is_online", function (username) {
   let child = document.createElement("li");
   child.classList.add("mx-auto", "joined-cl");
-  let italic = document.createElement("i");
-  italic.innerText = `${username} has joined the chat`;
-  child.appendChild(italic);
+  child.innerHTML = username;
   messagesEl.appendChild(child);
-  textInputEl.setAttribute("placeholder", `type as ${username}`);
+  
 });
 
 // ask username
+const validUser = function (userInfo) {
+  let userId = userInfo.trim();
+  if (
+      userId === "" ||
+      userId === null ||
+      userId === undefined ||
+      userId === "undefined" ||
+      userId.length > 10
+  ) {
+      let mat = Math.floor(Math.random() * 10) + 1;
+      let str = Math.random().toString(36).substr(2, 3);
+      return `user-${mat}${str}`;
+  } else {
+      return userInfo;
+  }
+};
+
 var userPrompt = prompt("Please tell me your name");
-let username = userPrompt.trim();
+let username = validUser(userPrompt);
+textInputEl.setAttribute("placeholder", `type as ${username}`);
 socket.emit("username", username);
